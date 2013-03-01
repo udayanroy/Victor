bf9622d2fe876a2f60a67a48f7316d58187cb8af
udayan roy <uroy@bitbucket.org>
2013-03-01 10:51:39 +0530

    victor praimary file upload compleate

---
+++ src/Victor/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.ImageToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.HwjToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 24)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(652, 389)
+        Me.Panel1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ImageToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(652, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ImageToolStripMenuItem
+        '
+        Me.ImageToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HwjToolStripMenuItem})
+        Me.ImageToolStripMenuItem.Name = "ImageToolStripMenuItem"
+        Me.ImageToolStripMenuItem.Size = New System.Drawing.Size(47, 20)
+        Me.ImageToolStripMenuItem.Text = "image"
+        '
+        'HwjToolStripMenuItem
+        '
+        Me.HwjToolStripMenuItem.Name = "HwjToolStripMenuItem"
+        Me.HwjToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HwjToolStripMenuItem.Text = "hwj"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(652, 413)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Child"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ImageToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HwjToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ src/Victor/view.vb
@@ -0,0 +1,19 @@
+﻿Public Class view
+    Dim core As vCore.vCore
+    Dim pagesz As New Size(500, 500)
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub view_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        core = New vCore.vCore(Panel1, pagesz)
+    End Sub
+
+   
+   
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        core.selectTool(CInt(TextBox1.Text))
+    End Sub
+End Class
