c733f333b2b4539d3ee3c4edf4aec0e5dfba038b
udayan <udaroy@rediffmail.com>
2013-08-09 09:57:01 +0530

    just change and experimenting

--- src/vCore/displayDevice/advancedPanel.vb
+++ src/vCore/displayDevice/advancedPanel.vb
@@ -12,12 +12,12 @@ Public Class advancedPanel
         'Me.SetStyle(ControlStyles.DoubleBuffer, True)
         'Me.SetStyle(ControlStyles.ResizeRedraw, True)
         'Me.SetStyle(ControlStyles.AllPaintingInWmPaint, True)
-        'Me.SetStyle(ControlStyles.UserPaint, True)
+        'Me.SetStyle(ControlStyles.UserPaint, true)
         Me.DoubleBuffered = True
 
     End Sub
     
-    
+   
 
 
     Public Sub setScrollminsizepos(ByVal position As Point, ByVal size As Size)
@@ -28,7 +28,7 @@ Public Class advancedPanel
 
     End Sub
     <DllImport("user32.dll")> _
-Private Shared Function SendMessage(ByVal hwnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
+    Private Shared Function SendMessage(ByVal hwnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
     End Function
     Private Const WM_SETREDRAW As Integer = &HB
     Public Sub suspendDraw()
@@ -39,4 +39,8 @@ Private Shared Function SendMessage(ByVal hwnd As IntPtr, ByVal wMsg As Integer,
         SendMessage(Me.Handle, WM_SETREDRAW, 1, 0)
 
     End Sub
+
+    
+
+   
 End Class
