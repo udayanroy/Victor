b252500d30e6f02838268dd8a95ddaed5c0fc674
Udayan Roy <udaroy@rediffmail.com>
2015-07-06 14:53:10 +0530

    pointer Remove tool structure added

---
+++ src/vCore/Core/Tool/PointerRemoveTool.vb
@@ -0,0 +1,60 @@
+﻿
+
+
+Public Class PointerRemoveTool
+    Implements Itool, Iedtr
+
+
+    Dim v As vCore
+    Dim WithEvents dc As advancedPanel
+    Public Sub New(ByRef vew As vCore)
+        v = vew
+    End Sub
+
+    Public Sub DeSelectTool() Implements Itool.DeSelectTool
+        dc = Nothing
+    End Sub
+
+    Public ReadOnly Property Device As advancedPanel Implements Itool.Device
+        Get
+            Return dc
+        End Get
+    End Property
+
+    Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
+        dc = d
+        v.Editor.setIEdit(Me)
+    End Sub
+
+
+
+    Private Sub dc_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles dc.MouseDown
+
+        Me.mouse_Down(e)
+    End Sub
+
+    Private Sub dc_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles dc.MouseMove
+
+        Me.mouse_Move(e)
+    End Sub
+    Private Sub dc_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles dc.MouseUp
+
+        Me.mouse_Up(e)
+    End Sub
+
+    Public Sub Draw(ByRef g As Drawing.Graphics) Implements Iedtr.Draw
+
+    End Sub
+
+    Public Sub mouse_Down(ByRef e As Windows.Forms.MouseEventArgs) Implements Iedtr.mouse_Down
+
+    End Sub
+
+    Public Sub mouse_Move(ByRef e As Windows.Forms.MouseEventArgs) Implements Iedtr.mouse_Move
+
+    End Sub
+
+    Public Sub mouse_Up(ByRef e As Windows.Forms.MouseEventArgs) Implements Iedtr.mouse_Up
+
+    End Sub
+End Class
