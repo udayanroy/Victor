a09ab368f42faca6d11360965056667e40a84860
Udayan Roy <udaroy@rediffmail.com>
2015-07-06 15:03:24 +0530

    visual of pointer Remove tool

--- src/vCore/Core/Tool/PointerRemoveTool.vb
+++ src/vCore/Core/Tool/PointerRemoveTool.vb
@@ -1,14 +1,21 @@
-﻿
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+
 
 
 Public Class PointerRemoveTool
     Implements Itool, Iedtr
 
 
-    Dim v As vCore
+    Dim Core As vCore
     Dim WithEvents dc As advancedPanel
-    Public Sub New(ByRef vew As vCore)
-        v = vew
+
+    Dim spath As vPath
+    Dim editablepath As GPath
+    Dim noderadious As Single = 3
+
+    Public Sub New(ByRef vcore As vCore)
+        Core = vcore
     End Sub
 
     Public Sub DeSelectTool() Implements Itool.DeSelectTool
@@ -23,7 +30,7 @@ Public Class PointerRemoveTool
 
     Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
         dc = d
-        v.Editor.setIEdit(Me)
+        Core.Editor.setIEdit(Me)
     End Sub
 
 
@@ -43,7 +50,16 @@ Public Class PointerRemoveTool
     End Sub
 
     Public Sub Draw(ByRef g As Drawing.Graphics) Implements Iedtr.Draw
-
+        If Core.Editor.selection.isEmty = False Then
+            g.SmoothingMode = SmoothingMode.AntiAlias
+            Using p As New Pen(Color.SkyBlue)
+                spath = Core.Editor.getSelectionPath()
+                editablepath = spath.GraphicsPath.Clone
+                Core.View.mem2DcGPath(editablepath)
+                editablepath.drawPath(g, p)
+                DrawNodes(g)
+            End Using
+        End If
     End Sub
 
     Public Sub mouse_Down(ByRef e As Windows.Forms.MouseEventArgs) Implements Iedtr.mouse_Down
@@ -57,4 +73,17 @@ Public Class PointerRemoveTool
     Public Sub mouse_Up(ByRef e As Windows.Forms.MouseEventArgs) Implements Iedtr.mouse_Up
 
     End Sub
+
+
+    Private Sub DrawNodes(g As System.Drawing.Graphics)
+        For Each sp As SubPath In editablepath.subpaths
+            For Each nd As PathPoint In sp.Points
+                g.FillEllipse(Brushes.White, nd.M.X - noderadious, nd.M.Y - noderadious, noderadious * 2, noderadious * 2)
+                g.DrawEllipse(New Pen(Brushes.Red, 2), nd.M.X - noderadious, nd.M.Y - noderadious, noderadious * 2, noderadious * 2)
+            Next
+        Next
+
+    End Sub
+ 
+
 End Class
