9bb6b60e63a7a06ab5978a540a6b1dfc2b6f46dc
Udayan Roy <udaroy@rediffmail.com>
2015-07-30 21:36:29 +0530

    Line Tool Added

---
+++ src/vCore/Core/Tool/LineTool.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Drawing
+
+Public Class LineTool
+    Implements Itool
+
+
+    Dim Core As vCore
+    Dim WithEvents dc As advancedPanel
+    Dim primaryLocation As Point
+
+
+    Public Sub New(ByRef vcore As vCore)
+        Core = vcore
+    End Sub
+
+    Public Sub DeSelectTool() Implements Itool.DeSelectTool
+        dc = Nothing
+    End Sub
+
+    Public ReadOnly Property Device As advancedPanel Implements Itool.Device
+        Get
+            Return dc
+        End Get
+    End Property
+
+    Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
+        dc = d
+    End Sub
+
+    Private Sub dc_MouseDown(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseDown
+        primaryLocation = e.Location
+        Core.View.BufferGraphics.Initialize()
+    End Sub
+
+    Private Sub dc_MouseMove(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Core.View.BufferGraphics.Clear()
+           
+            Core.View.BufferGraphics.Graphics.DrawLine(Pens.Black, primaryLocation.X, primaryLocation.Y, e.Location.X, e.Location.Y)
+            Core.View.BufferGraphics.Render()
+
+        End If
+    End Sub
+
+    Private Sub dc_MouseUp(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseUp
+
+
+        Dim vp As New vPath
+        vp.GraphicsPath.subpaths.Clear()
+        vp.GraphicsPath.AddLine(primaryLocation, e.Location)
+        'Convert path to memory path
+        Core.View.Dc2MemGPath(vp.GraphicsPath)
+        'Add it to Memory
+        Core.View.Memory.Layers(0).Item.Add(vp)
+
+        Core.View.Refresh()
+    End Sub
+End Class
