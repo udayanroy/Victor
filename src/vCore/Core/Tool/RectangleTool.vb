60290ef86137fc58c30a94543ad459b5a22645e2
Udayan Roy <udaroy@rediffmail.com>
2015-07-30 19:31:28 +0530

    Started to develop rectangle drawing tool

---
+++ src/vCore/Core/Tool/RectangleTool.vb
@@ -0,0 +1,27 @@
+﻿Public Class RectangleTool
+    Implements Itool
+
+
+    Dim Core As vCore
+    Dim WithEvents dc As advancedPanel
+
+  
+
+    Public Sub New(ByRef vcore As vCore)
+        Core = vcore
+    End Sub
+
+    Public Sub DeSelectTool() Implements Itool.DeSelectTool
+        dc = Nothing
+    End Sub
+
+    Public ReadOnly Property Device As advancedPanel Implements Itool.Device
+        Get
+            Return dc
+        End Get
+    End Property
+
+    Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
+        dc = d
+    End Sub
+End Class
