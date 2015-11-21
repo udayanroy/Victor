909b1080c0a554f5ecf6e4a1e2a7a7c42639fc7b
Udayan Roy <udaroy@rediffmail.com>
2015-11-21 23:39:17 +0530

    base class for various trasformation

---
+++ src/vCore/Geometry/Transform.vb
@@ -0,0 +1,25 @@
+﻿
+
+
+
+Namespace Geometry
+    Public MustInherit Class Transform
+
+        Public MustOverride ReadOnly Property Value As Matrix
+            
+        Public Sub TransformRect(ByRef rect As Rect)
+            rect.Transform(Me.Value)
+        End Sub
+
+        Public Sub TransformPoint(ByRef point As Point)
+            point.Transformation(Me.Value)
+        End Sub
+
+        Public Sub TransformPoints(ByRef points As Point())
+            Me.Value.map(points)
+        End Sub
+
+    End Class
+End Namespace
+
+
