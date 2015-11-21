83893e6df39ff9a27907679b6d61f7ab850265e3
Udayan Roy <udaroy@rediffmail.com>
2015-11-21 23:50:28 +0530

    MatrixTransform class added

---
+++ src/vCore/Geometry/MatrixTransform.vb
@@ -0,0 +1,30 @@
+﻿
+
+Namespace Geometry
+    Public Class MatrixTransform
+        Inherits Transform
+
+
+        Public Sub New()
+
+        End Sub
+        Public Sub New(m11 As Double, m12 As Double, m21 As Double,
+                 m22 As Double, dX As Double, dY As Double)
+
+            Me.Matix = New Matrix(m11, m12, m21, m22, dX, dY)
+
+        End Sub
+        Public Sub New(mat As Matrix)
+            Me.Matix = mat
+        End Sub
+
+        Public Property Matix As Matrix
+
+        Public Overrides ReadOnly Property Value As Matrix
+            Get
+                Return Me.Matix
+            End Get
+        End Property
+    End Class
+End Namespace
+
