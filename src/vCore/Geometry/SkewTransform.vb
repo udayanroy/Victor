a2bc8326d737f43c13c0f50737d67c4dc5d4fa67
Udayan Roy <udaroy@rediffmail.com>
2015-11-23 11:57:39 +0530

    skewTransform class added

---
+++ src/vCore/Geometry/SkewTransform.vb
@@ -0,0 +1,30 @@
+﻿
+
+
+Namespace Geometry
+    Public Class SkewTransform
+        Inherits Transform
+
+        Public Sub New()
+
+        End Sub
+
+        Public Sub New(SkewHorizontal As Double, SkewVertical As Double)
+            Me.SkewHorizontal = SkewHorizontal
+            Me.SkewVertical = SkewVertical
+        End Sub
+
+        Public Property SkewHorizontal As Double
+
+        Public Property SkewVertical As Double
+
+        Public Overrides ReadOnly Property Value As Matrix
+            Get
+                Dim mat = Matrix.Identity
+                mat.Shear(SkewHorizontal, SkewVertical)
+                Return mat
+            End Get
+        End Property
+    End Class
+End Namespace
+
