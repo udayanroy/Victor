8c6298c8d05aaedd9fd31e252e7fc41878097457
Udayan Roy <udaroy@rediffmail.com>
2015-12-09 15:01:03 +0530

    ElementBound Function

--- src/vCore/Core/DOM/DrawingElement.vb
+++ src/vCore/Core/DOM/DrawingElement.vb
@@ -10,12 +10,17 @@ Public Interface DrawingElement
     Function isBoundVisible(ByVal p As PointF) As Boolean
     Function GetElementType() As ElementType
 
-    'TransForm Function
-    Function GetBound() As RectangleF
+    'Get loosly claculated ElementBound
+    Function GetElementBound() As RectangleF
+
+    'Transform Function
+    Function GetItemBound() As RectangleF
     Sub Translate(ByVal x As Single, ByVal y As Single)
     Sub Resize(width As Single, height As Single)
+    Sub ReArrange(ByVal x As Single, ByVal y As Single, ByVal width As Single, ByVal height As Single)
     Property Rotation As Single
     Function GetSkeliton() As GPath
+    Sub ApplyTransform(mat As Drawing2D.Matrix)
 End Interface
 
 
