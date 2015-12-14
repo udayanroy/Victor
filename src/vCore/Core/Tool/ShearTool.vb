f97fbe17acbb06d82e6a2cb3424886b5b390d116
Udayan Roy <udaroy@rediffmail.com>
2015-12-14 17:47:57 +0530

    we are not using ShearTool. so we are not going to upgrade it

--- src/vCore/Core/Tool/ShearTool.vb
+++ src/vCore/Core/Tool/ShearTool.vb
@@ -3,156 +3,157 @@ Imports System.Windows.Forms
 Imports System.Drawing.Drawing2D
 
 Public Class ShearTool
-    Implements Itool, IEditor
+
+    'Implements Itool, IEditor
 
 
-    Dim Core As vCore
-    Dim WithEvents dc As advancedPanel
+    'Dim Core As vCore
+    'Dim WithEvents dc As advancedPanel
 
-    Dim noderadious As Single = 3
-    Private nodewidth As Integer = noderadious * 2
+    'Dim noderadious As Single = 3
+    'Private nodewidth As Integer = noderadious * 2
 
-    Dim s As Integer = 0
-    Dim svp As GPath
+    'Dim s As Integer = 0
+    'Dim svp As GPath
 
-    Dim MouseDownLocation As Point
-
-
-    Public Sub New(ByRef vcore As vCore)
-        Core = vcore
-    End Sub
-
-    Public Sub DeSelectTool() Implements Itool.DeSelectTool
-        dc = Nothing
-    End Sub
-
-    Public ReadOnly Property Device As advancedPanel Implements Itool.Device
-        Get
-            Return dc
-        End Get
-    End Property
-
-    Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
-        dc = d
-        Core.Editor.setIEdit(Me)
-    End Sub
-
-    Public Sub Draw(ByRef g As Graphics) Implements Iedtr.Draw
-        If Core.Editor.selection.isEmty = False Then
-            g.SmoothingMode = SmoothingMode.AntiAlias
-            Using p As New Pen(Color.Red), pth As New GraphicsPath
-
-                Dim rf As RectangleF = Core.Editor.getBoundRect()
-                pth.AddRectangle(rf)
-                Core.View.mem2DcPath(pth)
-
-                Dim bound = Rectangle.Round(pth.GetBounds)
-
-                g.DrawPath(p, pth)
-
-                Dim pointers() As Rectangle = {getRect(bound.X, bound.Y + bound.Height / 2), _
-                                      getRect(bound.X + bound.Width / 2, bound.Y), _
-                                      getRect(bound.X + bound.Width / 2, bound.Y + bound.Height), _
-                                      getRect(bound.X + bound.Width, bound.Y + bound.Height / 2)}
+    'Dim MouseDownLocation As Point
 
 
+    'Public Sub New(ByRef vcore As vCore)
+    '    Core = vcore
+    'End Sub
 
-                'Draw resize pointers
-                g.FillRectangles(Brushes.White, pointers)
-                g.DrawRectangles(Pens.Brown, pointers)
+    'Public Sub DeSelectTool() Implements Itool.DeSelectTool
+    '    dc = Nothing
+    'End Sub
+
+    'Public ReadOnly Property Device As advancedPanel Implements Itool.Device
+    '    Get
+    '        Return dc
+    '    End Get
+    'End Property
+
+    'Public Sub SelectTool(ByRef d As advancedPanel) Implements Itool.SelectTool
+    '    dc = d
+    '    Core.Editor.setIEdit(Me)
+    'End Sub
+
+    'Public Sub Draw(ByRef g As Graphics) Implements Iedtr.Draw
+    '    If Core.Editor.selection.isEmty = False Then
+    '        g.SmoothingMode = SmoothingMode.AntiAlias
+    '        Using p As New Pen(Color.Red), pth As New GraphicsPath
+
+    '            Dim rf As RectangleF = Core.Editor.getBoundRect()
+    '            pth.AddRectangle(rf)
+    '            Core.View.mem2DcPath(pth)
+
+    '            Dim bound = Rectangle.Round(pth.GetBounds)
+
+    '            g.DrawPath(p, pth)
+
+    '            Dim pointers() As Rectangle = {getRect(bound.X, bound.Y + bound.Height / 2), _
+    '                                  getRect(bound.X + bound.Width / 2, bound.Y), _
+    '                                  getRect(bound.X + bound.Width / 2, bound.Y + bound.Height), _
+    '                                  getRect(bound.X + bound.Width, bound.Y + bound.Height / 2)}
 
 
 
-            End Using
-        End If
-    End Sub
-
-    Public Sub mouse_Down(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Down
-        If MouseButtons.Left Then
+    '            'Draw resize pointers
+    '            g.FillRectangles(Brushes.White, pointers)
+    '            g.DrawRectangles(Pens.Brown, pointers)
 
 
-            If Core.Editor.selection.isEmty = False Then
-                Using pth As New GraphicsPath
-                    Dim rf As RectangleF = Core.Editor.getBoundRect()
-                    pth.AddRectangle(rf)
-                    Core.Editor.View.mem2DcPath(pth)
-                    Dim bound = Rectangle.Round(pth.GetBounds)
 
-                    Dim hit = Me.hittest(e.Location, bound)
+    '        End Using
+    '    End If
+    'End Sub
 
-                    If hit <> -1 Then
-                        'mdp = Me.MiddlePoint(bound)
-                        'mda = Me.Angle(mdp, e.Location)
-
-                        svp = Core.Editor.getSelectionPath.GraphicsPath.Clone
-                        Core.Editor.View.mem2DcGPath(svp)
-                        Core.Editor.View.BufferGraphics.Initialize()
-                         
-                    End If
-                End Using
-                
-            End If
-
-        End If
-    End Sub
-
-    Public Sub mouse_Move(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Move
-        If e.Button = MouseButtons.Left Then
-            '  Dim angl = Me.Angle(mdp, e.Location)
-            Core.Editor.View.BufferGraphics.Clear()
-            Using mat As New Matrix, tmpth As GraphicsPath = svp.ToGraphicsPath
-                ' mat.Shear()
-                tmpth.Transform(mat)
-                Core.Editor.View.BufferGraphics.Graphics.DrawPath(Pens.Brown, tmpth)
-                Core.Editor.View.BufferGraphics.Render()
-            End Using
-
-        End If
-    End Sub
-
-    Public Sub mouse_Up(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Up
-
-    End Sub
-
-    Private Sub dc_MouseDown(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseDown
-        Me.mouse_Down(e)
-    End Sub
-
-    Private Sub dc_MouseMove(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseMove
-        Me.mouse_Move(e)
-    End Sub
-
-    Private Sub dc_MouseUp(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseUp
-        Me.mouse_Up(e)
-    End Sub
+    'Public Sub mouse_Down(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Down
+    '    If MouseButtons.Left Then
 
 
-    Private Function getRect(ByVal x As Integer, ByVal y As Integer) As Rectangle
-        Return New Rectangle(x - noderadious, y - noderadious, nodewidth, nodewidth)
-    End Function
+    '        If Core.Editor.selection.isEmty = False Then
+    '            Using pth As New GraphicsPath
+    '                Dim rf As RectangleF = Core.Editor.getBoundRect()
+    '                pth.AddRectangle(rf)
+    '                Core.Editor.View.mem2DcPath(pth)
+    '                Dim bound = Rectangle.Round(pth.GetBounds)
 
-    Private Function getRect(ByVal x As Integer, ByVal y As Integer, radious As Integer) As Rectangle
-        Return New Rectangle(x - radious, y - radious, radious * 2, radious * 2)
-    End Function
+    '                Dim hit = Me.hittest(e.Location, bound)
 
-    Private Function hittest(ByVal p As Point, ByRef b As Rectangle) As Integer
+    '                If hit <> -1 Then
+    '                    'mdp = Me.MiddlePoint(bound)
+    '                    'mda = Me.Angle(mdp, e.Location)
 
-        Dim pointers() As Rectangle = {getRect(b.X, b.Y + b.Height / 2), _
-                                      getRect(b.X + b.Width / 2, b.Y), _
-                                      getRect(b.X + b.Width / 2, b.Y + b.Height), _
-                                      getRect(b.X + b.Width, b.Y + b.Height / 2)
-                                      }
+    '                    svp = Core.Editor.getSelectionPath.GraphicsPath.Clone
+    '                    Core.Editor.View.mem2DcGPath(svp)
+    '                    Core.Editor.View.BufferGraphics.Initialize()
+
+    '                End If
+    '            End Using
+
+    '        End If
+
+    '    End If
+    'End Sub
+
+    'Public Sub mouse_Move(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Move
+    '    If e.Button = MouseButtons.Left Then
+    '        '  Dim angl = Me.Angle(mdp, e.Location)
+    '        Core.Editor.View.BufferGraphics.Clear()
+    '        Using mat As New Matrix, tmpth As GraphicsPath = svp.ToGraphicsPath
+    '            ' mat.Shear()
+    '            tmpth.Transform(mat)
+    '            Core.Editor.View.BufferGraphics.Graphics.DrawPath(Pens.Brown, tmpth)
+    '            Core.Editor.View.BufferGraphics.Render()
+    '        End Using
+
+    '    End If
+    'End Sub
+
+    'Public Sub mouse_Up(ByRef e As MouseEventArgs) Implements Iedtr.mouse_Up
+
+    'End Sub
+
+    'Private Sub dc_MouseDown(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseDown
+    '    Me.mouse_Down(e)
+    'End Sub
+
+    'Private Sub dc_MouseMove(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseMove
+    '    Me.mouse_Move(e)
+    'End Sub
+
+    'Private Sub dc_MouseUp(sender As Object, e As Windows.Forms.MouseEventArgs) Handles dc.MouseUp
+    '    Me.mouse_Up(e)
+    'End Sub
 
 
-        Dim rtn As Integer = -1
+    'Private Function getRect(ByVal x As Integer, ByVal y As Integer) As Rectangle
+    '    Return New Rectangle(x - noderadious, y - noderadious, nodewidth, nodewidth)
+    'End Function
 
-        For i As Integer = 3 To 0 Step -1
-            If pointers(i).Contains(p) Then
-                rtn = i
-                Exit For
-            End If
-        Next
+    'Private Function getRect(ByVal x As Integer, ByVal y As Integer, radious As Integer) As Rectangle
+    '    Return New Rectangle(x - radious, y - radious, radious * 2, radious * 2)
+    'End Function
 
-        Return rtn
-    End Function
+    'Private Function hittest(ByVal p As Point, ByRef b As Rectangle) As Integer
+
+    '    Dim pointers() As Rectangle = {getRect(b.X, b.Y + b.Height / 2), _
+    '                                  getRect(b.X + b.Width / 2, b.Y), _
+    '                                  getRect(b.X + b.Width / 2, b.Y + b.Height), _
+    '                                  getRect(b.X + b.Width, b.Y + b.Height / 2)
+    '                                  }
+
+
+    '    Dim rtn As Integer = -1
+
+    '    For i As Integer = 3 To 0 Step -1
+    '        If pointers(i).Contains(p) Then
+    '            rtn = i
+    '            Exit For
+    '        End If
+    '    Next
+
+    '    Return rtn
+    'End Function
 End Class
--- src/vCore/Core/Tool/Tool.vb
+++ src/vCore/Core/Tool/Tool.vb
@@ -30,7 +30,7 @@ Public Class Tools
     'Dim TransformTool As TransformTool
     Dim SelectionTool As SelectionTool
     Dim ResizeTool As ResizeTool
-    Dim ShearTool As ShearTool
+    ' Dim ShearTool As ShearTool
 
     Public Sub New(ByRef vc As vCore)
         vcor = vc
@@ -52,7 +52,7 @@ Public Class Tools
         'TransformTool = New TransformTool(vcor)
         SelectionTool = New SelectionTool(vcor)
         ResizeTool = New ResizeTool(vcor)
-        ShearTool = New ShearTool(vcor)
+        ' ShearTool = New ShearTool(vcor)
 
         itl = pantool
         itl.SelectTool(vcor.pDevice)
@@ -103,7 +103,7 @@ Public Class Tools
             Case 17
                 itl = ResizeTool
             Case 18
-                itl = ShearTool
+                'itl = ShearTool
         End Select
                 itl.SelectTool(vcor.pDevice)
     End Sub
