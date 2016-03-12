5d0ca0ea5ee6d3dd6abb6be4e33f0bb0c6f0588d
Udayan Roy <udaroy@rediffmail.com>
2016-03-12 15:17:54 +0530

    Tool class is usable now

--- src/vCore/Core/Tool/Tool.vb
+++ src/vCore/Core/Tool/Tool.vb
@@ -1,35 +1,70 @@
-﻿
+﻿Imports Graphics
+
 
 Public Interface Itool
-
-    ReadOnly Property Device() As IDevice
     Sub DeSelectTool()
-    Sub SelectTool(ByRef d As IDevice)
+    Sub SelectTool(d As IDevice)
 End Interface
 
 Public MustInherit Class Tool
     Implements Itool
 
-   
-    Dim Core As vCore
-    Dim WithEvents dc As IDevice
-     
 
-    Public Sub New(ByRef vcore As vCore)
-        Core = vcore
+    Dim _core As vCore
+    Protected WithEvents _device As IDevice
+
+
+    Public Sub New(core As vCore)
+        Me._core = core
     End Sub
 
-    Public Sub DeSelectTool() Implements Itool.DeSelectTool
-
-    End Sub
-
-    Public ReadOnly Property Device As IDevice Implements Itool.Device
+    Protected ReadOnly Property Core As vCore
         Get
-
+            Return Me._core
         End Get
     End Property
 
-    Public Sub SelectTool(ByRef d As IDevice) Implements Itool.SelectTool
+    Protected ReadOnly Property Editor As Editor
+        Get
+            Return Me._core.Editor
+        End Get
+    End Property
+
+    Protected ReadOnly Property SelectionHolder As ISelectionHolder
+        Get
+            Return Me._core.Editor.SelectionHolder
+        End Get
+    End Property
+
+    Protected ReadOnly Property Visual As ControlVisual
+        Get
+            Return Me._core.View
+        End Get
+    End Property
+    Protected ReadOnly Property Dom As Document
+        Get
+            Return Me._core.View.DOM
+        End Get
+    End Property
+
+    Protected ReadOnly Property Device As IDevice
+        Get
+            Return Me._device
+        End Get
+    End Property
+
+    Public Overridable Sub DeSelectTool() Implements Itool.DeSelectTool
+        Me._device = Nothing
+    End Sub
+
+    Public Overridable Sub SelectTool(d As IDevice) Implements Itool.SelectTool
+        Me._device = d
 
     End Sub
+
+    Public Overridable Sub Draw(canvas As Canvas)
+
+    End Sub
+
+
 End Class
