7f9b28584af8d086a7510d8639f2e594c84fd232
Udayan Roy <udaroy@rediffmail.com>
2016-03-12 18:35:44 +0530

    base class for all capelement

---
+++ src/vCore/Core/Editor/CapElement.vb
@@ -0,0 +1,35 @@
+﻿
+
+Public MustInherit Class CapElement
+
+    Private _editor As Editor
+
+    Public Sub New(editor As Editor)
+        Me._editor = editor
+    End Sub
+
+    Protected ReadOnly Property Editor As Editor
+        Get
+            Return _editor
+        End Get
+    End Property
+
+    Protected ReadOnly Property Visual As ControlVisual
+        Get
+            Return Me.Editor.View
+        End Get
+    End Property
+
+    Protected ReadOnly Property Dom As Document
+        Get
+            Return Me.Editor.View.DOM
+        End Get
+    End Property
+
+    Protected ReadOnly Property SelectionHolder As ISelectionHolder
+        Get
+            Return Me.Editor.SelectionHolder
+        End Get
+    End Property
+
+End Class
