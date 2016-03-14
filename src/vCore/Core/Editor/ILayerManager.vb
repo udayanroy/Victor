0a3d22327286f4c520ffe76a93b93f0e4d1c1604
Udayan Roy <udaroy@rediffmail.com>
2016-03-15 01:24:28 +0530

    layer manager interface for layer ui

---
+++ src/vCore/Core/Editor/ILayerManager.vb
@@ -0,0 +1,12 @@
+﻿
+
+Public Interface ILayerManager
+
+    Property ActiveLayer As Layer
+
+    Sub AddLayer(LayrName As String)
+    Sub ActivateLayer(layer As Layer)
+    Function GetLayers() As IEnumerable(Of Layer)
+    Sub DeleteLayer()
+    Sub RepositionLayer(layer As Layer, Index As Integer)
+End Interface
