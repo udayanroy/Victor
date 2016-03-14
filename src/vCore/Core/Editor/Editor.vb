8b11b2411cb7806a9a8236e48640af8100a1fd25
Udayan Roy <udaroy@rediffmail.com>
2016-03-15 01:49:11 +0530

    dom could be accesable through editor

--- src/vCore/Core/Editor/Editor.vb
+++ src/vCore/Core/Editor/Editor.vb
@@ -6,21 +6,22 @@ Imports Graphics
 Public Class Editor
 
 
-    Dim vcor As vCore
-    Dim _SelectionHolder As SelectionHolder
+    Private _core As vCore
+    Private _SelectionHolder As SelectionHolder
     Private _layermanager As LayerManager
-    Dim ActiveTool As Tool
+    Private _ActiveTool As Tool
+
     Public Event SelectionChanged()
 
-    Public Sub New(ByRef v As vCore)
+    Public Sub New(v As vCore)
 
-        vcor = v
+        _core = v
         Me._SelectionHolder = New SelectionHolder(Me)
         Me._layermanager = New LayerManager(Me)
 
 
     End Sub
-   
+
     Public Function SelectAt(ByVal p As Point) As Integer
         Dim returnvalue As SelectionType
         returnvalue = Me.SelectionHolder.SelectAt(p)
@@ -42,21 +43,29 @@ Public Class Editor
 
     Public ReadOnly Property View As ControlVisual
         Get
-            Return vcor.View
+            Return _core.View
         End Get
 
     End Property
+
+    Public ReadOnly Property Dom As Document
+        Get
+            Return _core.View.DOM
+        End Get
+
+    End Property
+
     Public Sub Refresh()
-        vcor.View.Refresh()
+        _core.View.Refresh()
     End Sub
 
-    Public Sub SetActiveTool(tool As Tool)
-        Me.ActiveTool = tool
+    Friend Sub SetActiveTool(tool As Tool)
+        Me._ActiveTool = tool
     End Sub
 
-    Public Sub paint(canvas As Canvas)
-        If Me.ActiveTool IsNot Nothing Then
-            Me.ActiveTool.Draw(canvas)
+    Friend Sub paint(canvas As Canvas)
+        If Me._ActiveTool IsNot Nothing Then
+            Me._ActiveTool.Draw(canvas)
         End If
     End Sub
     Public ReadOnly Property LayerManager() As ILayerManager
