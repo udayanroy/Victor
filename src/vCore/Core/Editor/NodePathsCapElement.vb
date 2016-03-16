74b1b36a4b642b0053b8aa01241d5b20c832165c
Udayan Roy <udaroy@rediffmail.com>
2016-03-16 09:41:57 +0530

    NodePathsCapElement for path edit tools

---
+++ src/vCore/Core/Editor/NodePathsCapElement.vb
@@ -0,0 +1,13 @@
+﻿Imports Geometry
+
+Public Class NodePathsCapElement
+    Inherits CapElement
+
+    Public Sub New(editor As Editor)
+        MyBase.New(editor)
+    End Sub
+
+    Public Function GetSelectionSkeliton() As NodePath
+        Throw New NotImplementedException()
+    End Function
+End Class
