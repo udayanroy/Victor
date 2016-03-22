3cc9ae028dccdf3537ba431c8cbddb9ac4927c32
Udayan Roy <udaroy@rediffmail.com>
2016-03-22 20:47:57 +0530

    CapNode for edit Node directly

---
+++ src/vCore/Core/Editor/CapNode.vb
@@ -0,0 +1,53 @@
+﻿
+Imports Geometry
+
+Public Class CapNode
+    Inherits CapElement
+
+
+
+    Public Sub New(editor As Editor)
+        MyBase.New(editor)
+
+    End Sub
+
+    Public Property M As Point
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Point)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+    Public Property C1 As Point
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Point)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property C2 As Point
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Point)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property Type As NodeType
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As NodeType)
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Function Path() As NodePathsCapElement
+        Throw New NotImplementedException()
+    End Function
+
+End Class
--- src/vCore/Core/Editor/NodePathsCapElement.vb
+++ src/vCore/Core/Editor/NodePathsCapElement.vb
@@ -1,5 +1,9 @@
-﻿Imports Geometry
+﻿
+Imports Geometry
 
+''' <summary>
+''' 
+''' </summary>  
 Public Class NodePathsCapElement
     Inherits CapElement
 
@@ -7,7 +11,22 @@ Public Class NodePathsCapElement
         MyBase.New(editor)
     End Sub
 
+    Public Sub New(editor As Editor, Figure As NodeFigure)
+        MyBase.New(editor)
+
+        Throw New NotImplementedException()
+    End Sub
+    Public Sub New(editor As Editor, Path As NodePath)
+        MyBase.New(editor)
+
+        Throw New NotImplementedException()
+    End Sub
+
     Public Function GetSelectionSkeliton() As NodePath
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function GetNodePoints() As List(Of CapNode)
         Throw New NotImplementedException()
     End Function
 End Class
