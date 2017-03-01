df841a86c9c3f0cf9e3bf93c99db4aef77d57b5e
Udayan Roy <udaroy@rediffmail.com>
2017-03-01 20:50:36 +0530

    tPanTool base class upgraded

--- src/vCore/Core/Tool/tPanTool.vb
+++ src/vCore/Core/Tool/tPanTool.vb
@@ -2,42 +2,31 @@
 Imports Graphics
 
 Public Class tPanTool
-    Implements Itool
+    Inherits Tool
 
 
 
 
-    Dim core As vCore
-    Dim WithEvents dc As IDevice
-    Dim mdl As Point
+
+    Dim primaryLocation As Point
 
     Public Sub New(core As vCore)
-        Me.core = core
-    End Sub
-    Public ReadOnly Property Device() As IDevice Implements Itool.Device
-        Get
-            Return dc
-        End Get
-    End Property
-
-    Private Sub dc_MouseDown(e As MouseEvntArg) Handles dc.MouseDown
-        mdl = e.Location
+        MyBase.New(core)
     End Sub
 
-    Private Sub dc_MouseMove(e As MouseEvntArg) Handles dc.MouseMove
+
+    Protected Overrides Sub MouseDown(e As MouseEvntArg)
+        primaryLocation = e.Location
+    End Sub
+
+    Protected Overrides Sub MouseMove(e As MouseEvntArg)
         If e.Button = MouseButton.Left Then
 
-            core.View.panmove(New Point(mdl.X - e.Location.X, mdl.Y - e.Location.Y))
-            mdl = e.Location
+            Core.View.panmove(New Point(primaryLocation.X - e.Location.X, primaryLocation.Y - e.Location.Y))
+            primaryLocation = e.Location
 
         End If
     End Sub
 
-    Public Sub DeSelectTool() Implements Itool.DeSelectTool
-        dc = Nothing
-    End Sub
 
-    Public Sub SelectTool(ByRef d As IDevice) Implements Itool.SelectTool
-        dc = d
-    End Sub
 End Class
