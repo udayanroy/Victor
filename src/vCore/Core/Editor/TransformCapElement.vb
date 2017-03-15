a934d29aa530bdb9974ca0cb4f2e56657e68a869
Udayan Roy <udaroy@rediffmail.com>
2017-03-15 12:43:13 +0530

    Functions of TransformcapElement's are implemented

--- src/vCore/Core/Editor/TransformCapElement.vb
+++ src/vCore/Core/Editor/TransformCapElement.vb
@@ -10,19 +10,47 @@ Public Class TransformCapElement
     End Sub
 
     Public Function GetSelectionsBound() As Rect
+        Dim Bound = Me.SelectionHolder.SelectionList(0).Element.GetItemBound()
 
+        For Each ElementSelection In Me.SelectionHolder.SelectionList
+            Bound = Bound.union(ElementSelection.Element.GetItemBound())
+        Next
+        'Dim path As New NodePath
+        'path.AddRectangle(Bound)
+        'Editor.View.Memory2screen(path)
+        'Return path.GetBound
+        Editor.View.Memory2screen(Bound)
+
+        Return Bound
     End Function
 
     Public Function GetSelectionsBounds() As List(Of Rect)
-        Throw New NotImplementedException()
+        Dim rctlst As New List(Of Rect)
+        For Each ElementSelection In Me.SelectionHolder.SelectionList
+            Dim rct = ElementSelection.Element.GetItemBound()
+            Editor.View.Memory2screen(rct)
+            rctlst.Add(rct)
+        Next
+
+        Return rctlst
     End Function
 
     Public Function GetSelectionSkeliton() As NodePath
-        Throw New NotImplementedException()
+        ' !! upgrade code for multi selection !!
+        Dim SkelitonPath = SelectionHolder.SelectionList(0).Element.GetSkeliton
+        Editor.View.Memory2screen(SkelitonPath)
+        Return SkelitonPath
     End Function
 
     Public Sub ApplyTransform(transform As Transform)
-        Throw New NotImplementedException()
+        'Dim t = New TransformGroup
+
+        't.Items.Add(transform)
+        't.Items.Add(New MatrixTransform(Editor.))
+        'Me.SelectionHolder.SelectionList(0).Element.ApplyTransform(t)
+        'Dim mat = Editor.View.Screen2memory()  'Matrix.Identity
+        'mat.Multiply(transform.Value)
+        'Me.SelectionHolder.SelectionList(0).Element.ApplyTransform(mat)
     End Sub
 
     Public Property centerpoint As Point
