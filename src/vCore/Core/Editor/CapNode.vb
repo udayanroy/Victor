d991266d890e12c556f0cfc2a3a4ee050d1c61c4
Udayan Roy <udaroy@rediffmail.com>
2017-03-26 19:21:47 +0530

    primary method implementation of capnode

--- src/vCore/Core/Editor/CapNode.vb
+++ src/vCore/Core/Editor/CapNode.vb
@@ -5,44 +5,56 @@ Public Class CapNode
     Inherits CapElement
 
 
+    Dim _node As Node
+    Dim _figure As NodeFigure
+    Dim _path As New NodePath
 
-    Public Sub New(editor As Editor)
+    Public Sub New(editor As Editor, node As Node, figure As NodeFigure)
         MyBase.New(editor)
 
     End Sub
 
     Public Property M As Point
         Get
-            Throw New NotImplementedException()
+            Dim _m = _node.M
+            Editor.View.Memory2screen(_m)
+            Return _m
         End Get
         Set(value As Point)
-            Throw New NotImplementedException()
+            Editor.View.Screen2memory(value)
+            _node.M = value
         End Set
     End Property
     Public Property C1 As Point
         Get
-            Throw New NotImplementedException()
+            Dim _c1 = _node.M
+            Editor.View.Memory2screen(_c1)
+            Return _c1
         End Get
         Set(value As Point)
-            Throw New NotImplementedException()
+            Editor.View.Screen2memory(value)
+            _node.C1 = value
         End Set
     End Property
 
     Public Property C2 As Point
         Get
-            Throw New NotImplementedException()
+            Dim _c2 = _node.M
+            Editor.View.Memory2screen(_c2)
+            Return _c2
         End Get
         Set(value As Point)
-            Throw New NotImplementedException()
+            Editor.View.Screen2memory(value)
+            _node.C2 = value
         End Set
     End Property
 
     Public Property Type As NodeType
         Get
-            Throw New NotImplementedException()
+            Return _node.Type
         End Get
         Set(value As NodeType)
-            Throw New NotImplementedException()
+            _node.Type = value
         End Set
     End Property
 
